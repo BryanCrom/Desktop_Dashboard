1ec1c85efafa3a22efca3e5b8b7aeafa2322a0bc
Bryan <BryanCrombach@gmail.com>
2026-08-18 23:00:34 +1200

    feat(GamesPage.tsx): create REST API to get steam games from steam web api.

--- src/Pages/GamesPage.tsx
+++ src/Pages/GamesPage.tsx
@@ -1,4 +1,34 @@
+import { useEffect } from "react";
+import { fetch } from "@tauri-apps/plugin-http";
+
 const GamesPage = () => {
+  useEffect(() => {
+    const fetchSteamGames = async () => {
+      try {
+        const response = await fetch(
+          `https://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=${import.meta.env.VITE_STEAM_API_KEY}&steamid=76561198282312243&format=json`,
+          {
+            method: "GET",
+            headers: {
+              "Content-Type": "application/json",
+            },
+          },
+        );
+
+        if (!response.ok) {
+          throw new Error(`Status: ${response.status}`);
+        }
+
+        const data = await response.json();
+        console.log(data);
+      } catch (error) {
+        console.error("Error fetching steam games. " + error);
+      }
+    };
+
+    fetchSteamGames();
+  }, []);
+
   return <div>GamesPage</div>;
 };
 
